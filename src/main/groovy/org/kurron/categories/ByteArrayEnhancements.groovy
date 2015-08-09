d6f81f44564b104d7905a0a1e936d2d778daf5b9
Ron Kurr <kurr@kurron.org>
2015-08-09 15:16:16 +0000

    Added MD5 support.

---
+++ src/main/groovy/org/kurron/categories/ByteArrayEnhancements.groovy
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2015 Ronald D. Kurr kurr@jvmguy.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kurron.categories
+
+import org.apache.commons.codec.digest.DigestUtils
+
+/**
+ * Convenience methods that can be added to any byte array.
+ */
+class ByteArrayEnhancements {
+
+    /**
+     * Transforms the byte array in a hex representation of the buffer's MD5 hast.
+     * @param receiver the buffer to transform.
+     * @return 32 character hex string of the hash.
+     */
+    static String toMd5String( byte[] receiver ) {
+        DigestUtils.md5Hex( receiver )
+    }
+}
---
+++ src/test/groovy/org/kurron/categories/ByteArrayEnhancementsUnitTest.groovy
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2015 Ronald D. Kurr kurr@jvmguy.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kurron.categories
+
+import org.apache.commons.codec.digest.DigestUtils
+import org.junit.experimental.categories.Category
+import org.kurron.traits.GenerationAbility
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * Unit-level test.
+ */
+@Category( UnitTest )
+@Unroll
+class ByteArrayEnhancementsUnitTest extends Specification implements GenerationAbility {
+
+    def 'exercise MD5 string'() {
+        given: 'random data'
+        def data = randomByteArray( 128 )
+
+        when: 'enhanced method is applied'
+        def hash = use( ByteArrayEnhancements ) { ->
+            data.toMd5String()
+        }
+
+        then: 'the expected digest is generated'
+        DigestUtils.md5Hex( data ) == hash
+
+   }
+
+}
