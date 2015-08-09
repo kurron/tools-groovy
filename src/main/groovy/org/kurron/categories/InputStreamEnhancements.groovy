82b35e069bae7bd2e940a6742c61502aef47a5cf
Ron Kurr <kurr@kurron.org>
2015-08-09 16:20:05 +0000

    Added support for digesting and MAC'ing of streams.

---
+++ src/main/groovy/org/kurron/categories/InputStreamEnhancements.groovy
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2015 Ronald D. Kurr kurr@jvmguy.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kurron.categories
+
+import org.apache.commons.codec.digest.DigestUtils
+import org.apache.commons.codec.digest.HmacUtils
+
+/**
+ * Convenience methods that can be added to any input stream.
+ */
+class InputStreamEnhancements {
+
+    /**
+     * Transforms the stream into a hex representation of the stream's MD5 hash.
+     * @param receiver the stream to transform.
+     * @return 32 character hex string of the hash.
+     */
+    static String toMD5( InputStream receiver ) {
+        DigestUtils.md5Hex( receiver )
+    }
+
+    /**
+     * Transforms the stream into a hex representation of the stream's SHA-1 hash.
+     * @param receiver the buffer to transform.
+     * @return hex string of the hash.
+     */
+    static String toSha1( InputStream receiver ) {
+        DigestUtils.sha1Hex( receiver )
+    }
+
+    /**
+     * Transforms the stream into a hex representation of the stream's SHA-256 hash.
+     * @param receiver the buffer to transform.
+     * @return hex string of the hash.
+     */
+    static String toSha256( InputStream receiver ) {
+        DigestUtils.sha256Hex( receiver )
+    }
+
+    /**
+     * Transforms the stream into a hex representation of the stream's SHA-384 hash.
+     * @param receiver the buffer to transform.
+     * @return hex string of the hash.
+     */
+    static String toSha384( InputStream receiver ) {
+        DigestUtils.sha384Hex( receiver )
+    }
+
+    /**
+     * Transforms the stream into a hex representation of the stream's SHA-512 hash.
+     * @param receiver the buffer to transform.
+     * @return hex string of the hash.
+     */
+    static String toSha512( InputStream receiver ) {
+        DigestUtils.sha512Hex( receiver )
+    }
+
+    /**
+     * Transforms the stream into a hex representation of the stream's SHA-256 HMAC.
+     * @param receiver the buffer to transform.
+     * @param key the private key to use in the transformation.
+     * @return hex string of the HMAC.
+     */
+    static String toSha256Mac( InputStream receiver, byte[] key ) {
+        HmacUtils.hmacSha256Hex( key, receiver )
+    }
+
+    /**
+     * Transforms the stream into a hex representation of the stream's SHA-384 HMAC.
+     * @param receiver the buffer to transform.
+     * @param key the private key to use in the transformation.
+     * @return hex string of the HMAC.
+     */
+    static String toSha384Mac( InputStream receiver, byte[] key ) {
+        HmacUtils.hmacSha384Hex( key, receiver )
+    }
+
+    /**
+     * Transforms the stream into a hex representation of the stream's SHA-512 HMAC.
+     * @param receiver the buffer to transform.
+     * @param key the private key to use in the transformation.
+     * @return hex string of the HMAC.
+     */
+    static String toSha512Mac( InputStream receiver, byte[] key ) {
+        HmacUtils.hmacSha512Hex( key, receiver )
+    }
+}
---
+++ src/test/groovy/org/kurron/categories/InputStreamEnhancementsUnitTest.groovy
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) 2015 Ronald D. Kurr kurr@jvmguy.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kurron.categories
+
+import org.apache.commons.codec.digest.DigestUtils
+import org.apache.commons.codec.digest.HmacUtils
+import org.junit.experimental.categories.Category
+import org.kurron.traits.GenerationAbility
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * Unit-level test.
+ */
+@Category( UnitTest )
+@Unroll
+class InputStreamEnhancementsUnitTest extends Specification implements GenerationAbility {
+
+    def data = randomByteArray( 128 )
+    def stream = new ByteArrayInputStream( data )
+    def key = randomByteArray( 128 )
+
+    def 'exercise MD5 string'() {
+
+        when: 'enhanced method is applied'
+        def hash = use( InputStreamEnhancements ) { ->
+            stream.toMD5()
+        }
+
+        then: 'the expected digest is generated'
+        DigestUtils.md5Hex( data ) == hash
+   }
+
+    def 'exercise SHA-1 string'() {
+
+        when: 'enhanced method is applied'
+        def hash = use( InputStreamEnhancements ) { ->
+            stream.toSha1()
+        }
+
+        then: 'the expected digest is generated'
+        DigestUtils.sha1Hex( data ) == hash
+    }
+
+    def 'exercise SHA-256 string'() {
+
+        when: 'enhanced method is applied'
+        def hash = use( InputStreamEnhancements ) { ->
+            stream.toSha256()
+        }
+
+        then: 'the expected digest is generated'
+        DigestUtils.sha256Hex( data ) == hash
+    }
+
+    def 'exercise SHA-384 string'() {
+
+        when: 'enhanced method is applied'
+        def hash = use( InputStreamEnhancements ) { ->
+            stream.toSha384()
+        }
+
+        then: 'the expected digest is generated'
+        DigestUtils.sha384Hex( data ) == hash
+    }
+
+    def 'exercise SHA-512 string'() {
+
+        when: 'enhanced method is applied'
+        def hash = use( InputStreamEnhancements ) { ->
+            stream.toSha512()
+        }
+
+        then: 'the expected digest is generated'
+        DigestUtils.sha512Hex( data ) == hash
+    }
+
+    def 'exercise SHA-256 HMAC'() {
+
+        when: 'enhanced method is applied'
+        def mac = use( InputStreamEnhancements ) { ->
+            stream.toSha256Mac( key )
+        }
+
+        then: 'the expected authentication code is generated'
+        HmacUtils.hmacSha256Hex( key, data ) == mac
+    }
+
+    def 'exercise SHA-384 HMAC'() {
+
+        when: 'enhanced method is applied'
+        def mac = use( InputStreamEnhancements ) { ->
+            stream.toSha384Mac( key )
+        }
+
+        then: 'the expected authentication code is generated'
+        HmacUtils.hmacSha384Hex( key, data ) == mac
+    }
+
+    def 'exercise SHA-512 HMAC'() {
+
+        when: 'enhanced method is applied'
+        def mac = use( InputStreamEnhancements ) { ->
+            stream.toSha512Mac( key )
+        }
+
+        then: 'the expected authentication code is generated'
+        HmacUtils.hmacSha512Hex( key, data ) == mac
+    }
+}
